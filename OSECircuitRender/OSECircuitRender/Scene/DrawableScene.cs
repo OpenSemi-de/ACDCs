e2cfd0e4dcbe614c6be5098f118c4ae78bb6f710
lmnt1 <sb@lmnt.one>
2022-11-04 13:42:50 +0100

    Renderer Cleanup

--- OSECircuitRender/OSECircuitRender/Scene/DrawableScene.cs
+++ OSECircuitRender/OSECircuitRender/Scene/DrawableScene.cs
@@ -10,7 +10,6 @@ namespace OSECircuitRender.Scene
         public static float BaseGridSize = 2.54f;
         public static float Zoom = 10f;
         public SheetScene Scene { get; }
-        private ICanvas Canvas { get; set; }
 
         public DrawableScene(SheetScene scene)
         {
@@ -19,21 +18,17 @@ namespace OSECircuitRender.Scene
 
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
-            Canvas = canvas;
-            Canvas.FillColor = Colors.BlanchedAlmond;
-            Canvas.FillRectangle(0, 0, 10000, 10000);
+            canvas.FillColor = Colors.WhiteSmoke;
+            canvas.FillRectangle(0, 0, 10000, 10000);
             if (Scene == null) return;
             Scene.Drawables.ForEach(
-                drawable =>
-                {
-                    Render(drawable);
-                }
-                );
+                component => Render(canvas, component)
+            );
         }
 
-        public void Render(IDrawableComponent drawable)
+        public void Render(ICanvas canvas, IDrawableComponent drawable)
         {
-            Canvas.StrokeSize = BaseGridSize / 2;
+            canvas.StrokeSize = BaseGridSize / 2;
             GetScaleAndZoom(drawable, out Coordinate drawPos, out Coordinate drawSize);
 
             foreach (var instruction in drawable.DrawInstructions)
@@ -41,13 +36,8 @@ namespace OSECircuitRender.Scene
                 if (instruction is LineInstruction line)
                 {
                     Log.L("line");
-
-                    SetStrokeColor(Canvas, instruction.StrokeColor);
-                    Canvas.DrawLine(
-                        GetAbs(drawPos.X, drawSize.X, line.Position.X),
-                        GetAbs(drawPos.Y, drawSize.Y, line.Position.Y),
-                        GetAbs(drawPos.X, drawSize.X, line.End.X),
-                        GetAbs(drawPos.Y, drawSize.Y, line.End.Y));
+                    SetStrokeColor(canvas, instruction.StrokeColor);
+                    DrawLine(canvas, drawPos, drawSize, line);
                 }
 
                 if (instruction is BoxInstruction box)
@@ -55,94 +45,32 @@ namespace OSECircuitRender.Scene
                     Log.L("box");
                     var upperLeft = new Coordinate(box.Position);
                     var lowerRight = new Coordinate(box.Size);
-                    SetStrokeColor(Canvas, box.StrokeColor);
-                    SetFillColor(Canvas, box.FillColor);
-                    Canvas.DrawRectangle(
-                        GetAbs(drawPos.X, drawSize.X, upperLeft.X),
-                        GetAbs(drawPos.Y, drawSize.Y, upperLeft.Y),
-                        GetScale(drawSize.X, lowerRight.X),
-                        GetScale(drawSize.Y, lowerRight.Y));
+                    SetStrokeColor(canvas, box.StrokeColor);
+                    SetFillColor(canvas, box.FillColor);
+                    DrawRectangle(canvas, drawPos, drawSize, upperLeft, lowerRight);
                 }
 
                 if (instruction is TextInstruction text)
                 {
                     Log.L("text");
                     var centerPos = new Coordinate(instruction.Position);
-                    var x = GetAbs(drawPos.X, drawSize.X, centerPos.X);
-                    var y = GetAbs(drawPos.Y, drawSize.Y, centerPos.Y);
-                    Canvas.SaveState();
-                    Canvas.FontSize = text.Size;
-                    Canvas.Translate(x, y);
-                    Canvas.Rotate(text.Orientation);
-                    SetStrokeColor(Canvas, instruction.StrokeColor);
-                    Canvas.DrawString(text.Text, 0, 0, HorizontalAlignment.Center);
-                    Canvas.Translate(0, 0);
-                    Canvas.ResetState();
+                    SetStrokeColor(canvas, instruction.StrokeColor);
+                    DrawText(canvas, drawPos, drawSize, text, centerPos, instruction);
                 }
 
                 if (instruction is CircleInstruction)
                 {
                     Log.L("circle");
-                    var posCenter = new Coordinate(instruction.Position);
-                    posCenter.X = GetAbs(drawPos.X, drawSize.X, posCenter.X);
-                    posCenter.Y = GetAbs(drawPos.Y, drawSize.Y, posCenter.Y);
-                    SetStrokeColor(Canvas, instruction.StrokeColor);
-                    Canvas.DrawCircle(posCenter.X, posCenter.Y, Zoom * BaseGridSize * 0.1f);
+                    var centerPos = new Coordinate(instruction.Position);
+                    SetStrokeColor(canvas, instruction.StrokeColor);
+                    DrawCircle(canvas, centerPos, drawPos, drawSize);
                 }
 
                 if (instruction is PathInstruction path)
                 {
-                    SetStrokeColor(Canvas, instruction.StrokeColor);
-                    PathInstruction scenePath = path;
-
-                    PathF pathF = new(drawPos.X, drawPos.Y);
-
-                    foreach (var part in path.GetParts())
-                    {
-                        switch (part.Type)
-                        {
-                            case PathPartType.A:
-                                break;
-
-                            case PathPartType.C:
-                                break;
-
-                            case PathPartType.M:
-                                {
-                                    pathF.MoveTo(drawPos.X + part.Coordinates[0].X, drawPos.Y + part.Coordinates[0].Y);
-                                }
-                                break;
-
-                            case PathPartType.L:
-                                {
-                                    pathF.LineTo(drawPos.X + part.Coordinates[0].X, drawPos.Y + part.Coordinates[0].Y);
-                                }
-                                break;
-
-                            case PathPartType.H:
-                                break;
-
-                            case PathPartType.V:
-                                break;
-
-                            case PathPartType.S:
-                                break;
-
-                            case PathPartType.Q:
-                                break;
-
-                            case PathPartType.T:
-                                break;
-
-                            case PathPartType.Z:
-                                break;
-
-                            default:
-                                break;
-                        }
-                    }
-
-                    Canvas.DrawPath(pathF);
+                    Log.L("path");
+                    SetStrokeColor(canvas, instruction.StrokeColor);
+                    DrawPath(canvas, drawPos, path);
                 }
             }
 
@@ -152,9 +80,100 @@ namespace OSECircuitRender.Scene
                 var posCenter = new Coordinate(pin.Position);
                 posCenter.X = GetAbs(drawPos.X, drawSize.X, posCenter.X);
                 posCenter.Y = GetAbs(drawPos.Y, drawSize.Y, posCenter.Y);
-                SetStrokeColor(Canvas, pin.DrawInstructions[0].StrokeColor);
-                Canvas.DrawCircle(posCenter.X, posCenter.Y, Zoom * BaseGridSize * 0.1f);
+                SetStrokeColor(canvas, pin.DrawInstructions[0].StrokeColor);
+                canvas.DrawCircle(posCenter.X, posCenter.Y, Zoom * BaseGridSize * 0.1f);
             }
+        }
+
+        private static void DrawPath(ICanvas canvas, Coordinate drawPos, PathInstruction path)
+        {
+            PathF pathF = new(drawPos.X, drawPos.Y);
+
+            foreach (var part in path.GetParts())
+            {
+                switch (part.Type)
+                {
+                    case PathPartType.A:
+                        break;
+
+                    case PathPartType.C:
+                        break;
+
+                    case PathPartType.M:
+                        {
+                            pathF.MoveTo(drawPos.X + part.Coordinates[0].X, drawPos.Y + part.Coordinates[0].Y);
+                        }
+                        break;
+
+                    case PathPartType.L:
+                        {
+                            pathF.LineTo(drawPos.X + part.Coordinates[0].X, drawPos.Y + part.Coordinates[0].Y);
+                        }
+                        break;
+
+                    case PathPartType.H:
+                        break;
+
+                    case PathPartType.V:
+                        break;
+
+                    case PathPartType.S:
+                        break;
+
+                    case PathPartType.Q:
+                        break;
+
+                    case PathPartType.T:
+                        break;
+
+                    case PathPartType.Z:
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            canvas.DrawPath(pathF);
+        }
+
+        private static void DrawCircle(ICanvas canvas, Coordinate centerPos, Coordinate drawPos, Coordinate drawSize)
+        {
+            var x = GetAbs(drawPos.X, drawSize.X, centerPos.X);
+            var y = GetAbs(drawPos.Y, drawSize.Y, centerPos.Y);
+            canvas.DrawCircle(x, y, Zoom * BaseGridSize * 0.1f);
+        }
+
+        private static void DrawText(ICanvas canvas, Coordinate drawPos, Coordinate drawSize, TextInstruction text, Coordinate centerPos, IDrawInstruction instruction)
+        {
+            var x = GetAbs(drawPos.X, drawSize.X, centerPos.X);
+            var y = GetAbs(drawPos.Y, drawSize.Y, centerPos.Y);
+            canvas.SaveState();
+            canvas.FontSize = text.Size;
+            canvas.Translate(x, y);
+            canvas.Rotate(text.Orientation);
+            canvas.DrawString(text.Text, 0, 0, HorizontalAlignment.Center);
+            canvas.Translate(0, 0);
+            canvas.ResetState();
+        }
+
+        private static void DrawRectangle(ICanvas canvas, Coordinate drawPos, Coordinate drawSize, Coordinate upperLeft,
+            Coordinate lowerRight)
+        {
+            canvas.DrawRectangle(
+                GetAbs(drawPos.X, drawSize.X, upperLeft.X),
+                GetAbs(drawPos.Y, drawSize.Y, upperLeft.Y),
+                GetScale(drawSize.X, lowerRight.X),
+                GetScale(drawSize.Y, lowerRight.Y));
+        }
+
+        private static void DrawLine(ICanvas canvas, Coordinate drawPos, Coordinate drawSize, LineInstruction line)
+        {
+            canvas.DrawLine(
+                GetAbs(drawPos.X, drawSize.X, line.Position.X),
+                GetAbs(drawPos.Y, drawSize.Y, line.Position.Y),
+                GetAbs(drawPos.X, drawSize.X, line.End.X),
+                GetAbs(drawPos.Y, drawSize.Y, line.End.Y));
         }
 
         private static void GetScaleAndZoom(IDrawableComponent drawable, out Coordinate drawPos, out Coordinate drawSize)

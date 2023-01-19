15a25f5d486636232e88f2820c3097d43c399a69
lmnt1 <sb@lmnt.one>
2023-01-19 13:55:02 +0100

    Refactoring

--- ACDCS.CircuitRenderer/Items/SourceItem.cs
+++ ACDCS.CircuitRenderer/Items/SourceItem.cs
@@ -8,6 +8,12 @@ public class SourceItem : WorksheetItem
 
     public override bool IsInsertable => false;
 
+    public SourceItem()
+    {
+        DrawableComponent = new SourceDrawable(this, DefaultValue, SourceDrawableType.Voltage, 1, 1);
+        Value = DefaultValue;
+    }
+
     public SourceItem(SourceDrawableType sourceDrawableType)
     {
         DrawableComponent = new SourceDrawable(this, DefaultValue, sourceDrawableType, 1, 1);

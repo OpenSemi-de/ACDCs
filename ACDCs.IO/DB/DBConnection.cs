8035737f8de19819205205baba2b1deb41450958
lmnt1 <sb@lmnt.one>
2023-01-21 23:28:18 +0100

    Added preferences

--- ACDCs.IO/DB/DBConnection.cs
+++ ACDCs.IO/DB/DBConnection.cs
@@ -1,8 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using ACDCs.Data.ACDCs.Components.BJT;
-using ACDCs.Data.ACDCs.Components.Diode;
-using ACDCs.Data.ACDCs.Interfaces;
-using LiteDB;
+﻿using LiteDB;
 
 namespace ACDCs.IO.DB;
 
@@ -20,6 +16,24 @@ public class DBConnection
         _connectionString = $"Filename={dbdir}";
     }
 
+    public T? GetOrSet<T>(string collectionName, string keyName, string keyValue, T? newValue = default)
+    {
+        using LiteDatabase db = new(_connectionString);
+        T? retValue = default;
+        if (db.CollectionExists(collectionName))
+        {
+            var col = db.GetCollection<T>(collectionName);
+            retValue = col.FindOne(Query.EQ(keyName, keyValue));
+            if (newValue != null)
+            {
+                col.DeleteMany(Query.EQ(keyName, keyValue));
+                col.Insert(newValue);
+            }
+        }
+
+        return retValue;
+    }
+
     public List<T> Read<T>(string collectionName)
     {
         using LiteDatabase db = new(_connectionString);
@@ -33,51 +47,5 @@ public class DBConnection
         db.GetCollection<T>(collectionName)
             .Insert(items);
         db.Dispose();
-    }
-}
-
-[SuppressMessage("ReSharper", "UnusedMember.Global")]
-public class DefaultModelRepository
-{
-    private readonly DBConnection _connection;
-
-    public DefaultModelRepository()
-    {
-        _connection = new DBConnection("default");
-    }
-
-    public List<T> GetModels<T>(string type)
-    {
-        List<IElectronicComponent> models = new();
-
-        switch (type.ToLower())
-        {
-            case "pnp":
-            case "npn":
-                models = GetModels()
-                    .Where(c => c is Bjt bjt && bjt.TypeName == type.ToLower())
-                    .ToList();
-                break;
-
-            case "diode":
-                models = GetModels()
-                    .Where(c => c is Diode)
-                    .ToList();
-                break;
-        }
-
-        return models.Cast<T>().ToList();
-    }
-
-    public List<IElectronicComponent> GetModels()
-    {
-        List<IElectronicComponent> models = _connection.Read<IElectronicComponent>("Components");
-        return models;
-    }
-
-    public void Write(List<IElectronicComponent?> newComponents)
-    {
-        newComponents = newComponents.Where(c => c != null).ToList();
-        _connection.Write(newComponents, "Components");
     }
 }
